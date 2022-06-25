858046d22161729985e5debe4f1f5f2f11ed8080
dan <dhtonge@gmail.com>
2022-06-25 13:02:34 -0400

    read ics file

--- ical.rb
+++ ical.rb
@@ -27,3 +27,11 @@ def merged_feed
     cal.publish # ??
     cal.to_ical
 end
+
+def read_ics_file(filename)
+    g = open(filename,'rb')
+    gcal = Icalendar::Calendar.from_ical(g.read())
+    for component in gcal.walk():
+        print component.name
+    g.close()
+end
